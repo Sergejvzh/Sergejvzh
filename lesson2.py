496ecb47f1bcbe1ad22389b9cc2b1a7db5b94c03
Sergejvzh <Zhilenkov.72@mail.ru>
2024-11-25 14:39:56 +0300

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,8 @@
+name = "Sergej"
+print (name, type (name))
+age = 52
+print (age, type (age))
+new_age = '17'
+print (age + new_age))
+is _ student = 'True'
+print (is _ student, type (is _ student))
